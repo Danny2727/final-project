d2c7855f599ae54d302498f6370c1d159ccec1aa
Danny2727 <dannyedouard14@gmail.com>
2024-01-25 18:28:39 -0500

    created Rank.jsx in components folder

---
+++ src/Rank.jsx
@@ -0,0 +1,9 @@
+const Rank = () => {
+    return (
+        <div class="demon rank"> 
+        <p></p>
+        </div>
+    )
+    }
+    
+    export default Slayers;
---
+++ src/components/Chracter.jsx
@@ -0,0 +1,19 @@
+const Character = () => {
+    return(
+        <div class="chracter-list">
+        <div class="posters">
+            <img/>
+            <h2></h2>
+            <h4></h4>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+
+
+        </div>
+    )
+}
