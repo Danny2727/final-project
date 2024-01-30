5f02292c9bdc731918e20b277004ad4f458f53c4
Danny2727 <dannyedouard14@gmail.com>
2024-01-29 19:31:45 -0500

    added styling to the slayer, ul, and images

--- src/components/Slayers.jsx
+++ src/components/Slayers.jsx
@@ -7,12 +7,14 @@ const Slayers = () => {
                 <div>
                     <img src={slayer.image} />
                     <h2>{slayer.name}</h2>
-                    <ul>
+                    <div className="slayer-attributes">
+                    <ul className="abilities">
                         <li>{slayer.breathingstyle}</li>
                         <li>{slayer.abilities}</li>
                         <li>{slayer.fightstyle}</li>
                         <li>{slayer.techniques}</li>
                     </ul>
+                    </div>
                 </div>
             ))}
 
